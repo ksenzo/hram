a35786af16d0c0975055c2e8d0fbf863e394d683
ksenzo <ksenz.gleb61@gmail.com>
2022-01-22 21:23:58 +0200

    create and adaptive second section

---
+++ docs/js/script.js
@@ -0,0 +1,35 @@
+let openMenu = document.querySelector('.header__btn-mobile');
+let menuMobile = document.querySelector('.mobile-menu');
+let openMenuSpan = document.querySelector('.header__span');
+
+if (openMenu) {
+   openMenu.addEventListener('click', () => {
+      openMenuSpan.classList.toggle('__active');
+      menuMobile.classList.toggle('__active');
+   });
+}
+
+
+window.onscroll = function showHeader() {
+   let fixedHeader = document.querySelector('.header');
+   if (window.pageYOffset > 0) {
+      fixedHeader.classList.add('__active');
+   } else {
+      fixedHeader.classList.remove('__active');
+   }
+};
+
+
+const swiper = new Swiper('.slider', {
+   spaceBetween: 0,
+   slidesPerView: 'auto',
+   centerSlide: true,
+   pagination: {
+      el: '.slider-pagination',
+   },
+   allowTouchMove: true,
+   //navigation: {
+   //   nextEl: '.steps__step-button-next',
+   //   prevEl: '.steps__step-button-prev',
+   //},
+});
